ec0700b8bd5eba87fdb4eaf60cf09452f27ee2bf
Marijn Haverbeke <marijn@haverbeke.nl>
2022-03-30 09:03:50 +0200

    Clean up check in syntaxFolding

--- src/fold.ts
+++ src/fold.ts
@@ -24,7 +24,7 @@ export function foldInside(node: SyntaxNode): {from: number, to: number} | null 
 
 function syntaxFolding(state: EditorState, start: number, end: number) {
   let tree = syntaxTree(state)
-  if (tree.length == 0) return null
+  if (tree.length < end) return null
   let inner = tree.resolveInner(end)
   let found: null | {from: number, to: number} = null
   for (let cur: SyntaxNode | null = inner; cur; cur = cur.parent) {
